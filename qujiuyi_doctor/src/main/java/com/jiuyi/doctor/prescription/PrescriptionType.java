e30b6088eb30f180c80d3607a7cd125026d09a32
xutaoyang <xutaoyang@5078b89a-a44e-4b93-8557-55746213ff9f>
2015-12-17 14:16:53 +0000

    处方类型

---
+++ qujiuyi_doctor/src/main/java/com/jiuyi/doctor/prescription/PrescriptionType.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.jiuyi.doctor.prescription;
+
+/**
+ * 
+ * @author xutaoyang
+ *
+ */
+public enum PrescriptionType {
+
+	COMMON("普通药品处方");
+
+	public final String name;
+
+	/**
+	 * 
+	 */
+	private PrescriptionType(String name) {
+		this.name = name;
+	}
+
+	public static String getNameById(int type) {
+		if (type < 0 || type > values().length - 1) {
+			return null;
+		}
+		return values()[type].name;
+	}
+}
