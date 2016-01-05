376d953df18759ed9db5e43dfee7de41339381e7
xutaoyang <xutaoyang@5078b89a-a44e-4b93-8557-55746213ff9f>
2016-01-05 04:03:25 +0000

    评论数量join患者表

--- qujiuyi_doctor/src/main/java/com/jiuyi/doctor/comment/CommentDao.java
+++ qujiuyi_doctor/src/main/java/com/jiuyi/doctor/comment/CommentDao.java
@@ -16,7 +16,7 @@ public class CommentDao extends DbBase {
 
 	private static final String SELECT_EVALUATION_PAGE = "SELECT c.*,p.`name` as patientName,p.`gender` as patientGender, p.`headPortrait`as patientHead,p.`birthday` as patientAge FROM `t_doctor_comment` c,`t_patient` p WHERE p.`id`=c.`patientId` AND c.`doctorId`=? ORDER BY c.`commentTime` DESC LIMIT ?,?";
 	private static final String SELECT_EVALUATION_COUNT = "SELECT COUNT(`id`) FROM `t_doctor_comment` WHERE `commentTime`>? AND `doctorId`=?";
-	private static final String SELECT_DOCTOR_EVALUATION_COUNT = "SELECT COUNT(`id`) FROM `t_doctor_comment` WHERE `doctorId`=?";
+	private static final String SELECT_DOCTOR_EVALUATION_COUNT = "SELECT COUNT(cmt.`id`) FROM `t_doctor_comment` cmt,`t_patient` patient WHERE cmt.patientId=patient.id AND cmt.`doctorId`=?";
 
 	protected List<Comment> loadComment(Doctor doctor, int page, int pageSize) {
 		return queryForList(SELECT_EVALUATION_PAGE, new Object[] { doctor.getId(), startIndex(page, pageSize), pageSize }, Comment.class);
