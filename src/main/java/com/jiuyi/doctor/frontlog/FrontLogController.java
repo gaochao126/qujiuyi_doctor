92fd0967e1af1ae10f39288a095081b82fd0f62d
xutaoyang <xutaoyang@5078b89a-a44e-4b93-8557-55746213ff9f>
2016-01-30 02:20:39 +0000

    前端日志记录

---
+++ src/main/java/com/jiuyi/doctor/frontlog/FrontLogController.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.jiuyi.doctor.frontlog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jiuyi.frame.annotations.Param;
+import com.jiuyi.frame.front.ServerResult;
+
+/**
+ * 收集前端日志
+ * 
+ * @author xutaoyang
+ *
+ */
+@RestController
+public class FrontLogController {
+
+	private static final String CMD = "log_";
+	private static final String CMD_LOG = CMD + "add";
+
+	private @Autowired FrontLogManager manager;
+
+	@RequestMapping(CMD_LOG)
+	public ServerResult log(@Param("log") FrontLog log) {
+		return manager.insertLog(log);
+	}
+
+}
